010cc8b8aca7a0cd7b6b34c2f395a2e6616aefd5
McTechie <mc.soans02@gmail.com>
2022-07-09 15:02:46 +0530

    feat: create app header

---
+++ src/components/Categories.jsx
@@ -0,0 +1,12 @@
+import { View, Text, ScrollView } from 'react-native'
+import React from 'react'
+
+const Categories = () => {
+  return (
+    <ScrollView>
+      <Text>Categories</Text>
+    </ScrollView>
+  )
+}
+
+export default Categories;
---
+++ src/screens/HomeScreen.jsx
@@ -0,0 +1,53 @@
+import { View, Text, SafeAreaView, Image, TextInput, ScrollView } from 'react-native';
+import React from 'react';
+import { AdjustmentsIcon, ChevronDownIcon, SearchIcon, UserCircleIcon } from 'react-native-heroicons/outline';
+import Categories from '../components/Categories';
+
+const HomeScreen = () => {
+  return (
+    <SafeAreaView className='bg-white pt-5'>
+      {/* Header */}
+      <View className='flex-row pb-3 items-center mx-4 space-x-2'>
+        <Image
+          source={require('../../assets/techiemato.png')}
+          className='h-7 w-7 bg-gray-300 p-4 rounded-full'
+        />
+
+        <View className='flex-1'>
+          <Text className='font-bold text-gray-400 text-xs'>
+            Deliver Now!
+          </Text>
+          <Text className='font-bold text-xl'>
+            Current Location
+            <ChevronDownIcon size={20} color='#f43f5e' />
+          </Text>
+        </View>
+
+        <UserCircleIcon size={35} color='#f43f5e' />
+      </View>
+
+      {/* Search */}
+      <View className='flex-row items-center space-x-2 pb-2 mx-4'>
+        <View className='flex-row flex-1 space-x-2 bg-gray-200 p-3'>
+          <SearchIcon size={20} color='gray' />
+          <TextInput placeholder='Restaurants and cuisines' keyboardType='default' />
+        </View>
+        <AdjustmentsIcon color='#f43f5e' />
+      </View>
+
+      {/* Feed */}
+      <ScrollView
+        className='bg-gray-100'
+        contentContainerStyle={{
+          paddingBottom: 100
+        }}
+      >
+        <Categories />
+
+        {/* 3 Featured Rows */}
+      </ScrollView>
+    </SafeAreaView>
+  )
+}
+
+export default HomeScreen
